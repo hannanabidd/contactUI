16b9c96cba1f077b0bb7bca0c18ba341c9192388
aurangg <aurangzebbbk@gmail.com>
2021-09-30 14:19:55 +0500

    Changes by Aurang

--- src/ContactInfo/Banner.js
+++ src/ContactInfo/Banner.js
@@ -4,7 +4,7 @@ import React, { Fragment } from "react";
 function Banner (){
     return(
         <Fragment>
-            <img src="/images/Group 29.png" className="profile-img" alt="" />
+            <img src="/images/main-img.png" className="profile-img" alt="" />
                             <h1 className="banner-heading padding-lr">Amir Miri</h1>
                             <p className="banner-p padding-lr">REALTER</p>
                             <p className="banner-p padding-lr">Amir Miri PREC*</p>
--- src/ContactInfo/Info.js
+++ src/ContactInfo/Info.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import {Link} from 'react-router-dom'
 function Info() {
     return(
         <Fragment>
@@ -7,7 +6,7 @@ function Info() {
                 <a href="tel:+1 604 657 5030">
                     <div className="rounded-box">
                         <div className="icon-img">
-                                <img src="/images/Group 4.svg" className="" alt="PhoneNo"/>
+                                <img src="/images/call.svg" className="" alt="PhoneNo"/>
                         </div>
                         <div className="contact-info">
                             <p className="info-p">+1 604 657 5030</p>
@@ -17,37 +16,37 @@ function Info() {
                 <a href="mailto:amir@amirmiri.com">
                     <div className="rounded-box">
                         <div className="icon-img">
-                                <img src="/images/Group 5.svg" className="" alt="gmail"/>
+                                <img src="/images/mail.svg" className="" alt="gmail"/>
                         </div>
                         <div className="contact-info">
                             <p className="info-p">amir@amirmiri.com</p>
                         </div>
                     </div>
                 </a>
-                <a href="https://amirmiri.com/">
+                <a href="https://amirmiri.com/" target="_blank" rel="noreferrer">
                     <div className="rounded-box">
                         <div className="icon-img">
-                                <img src="/images/Group 6.svg" className="" alt="website"/>
+                                <img src="/images/website.svg" className="" alt="website"/>
                         </div>
                         <div className="contact-info">
                             <p className="info-p">www.amirmiri.com</p>
                         </div>
                     </div>
                 </a>
-                <a href="https://www.instagram.com/amirhomes/">
+                <a href="https://www.instagram.com/amirhomes/" target="_blank" rel="noreferrer">
                     <div className="rounded-box">
                         <div className="icon-img">
-                                <img src="/images/Group 7.svg" className="" alt="instagram"/>
+                                <img src="/images/instagram.svg" className="" alt="instagram"/>
                         </div>
                         <div className="contact-info">
                             <p className="info-p">@amirhomes</p>
                         </div>
                     </div>
                 </a>
-                <a href="https://api.whatsapp.com/send?phone=16046575030">
+                <a href="https://api.whatsapp.com/send?phone=16046575030" target="_blank" rel="noreferrer">
                     <div className="rounded-box">
                         <div className="icon-img">
-                                <img src="/images/Group 8.svg" className="" alt="whatsapp"/>
+                                <img src="/images/whatsapp.svg" className="" alt="whatsapp"/>
                         </div>
                         <div className="contact-info">
                             <p className="info-p">6046575030</p>
@@ -57,44 +56,44 @@ function Info() {
                 <a href="">
                     <div className="rounded-box">
                         <div className="icon-img">
-                                <img src="/images/Group 9.svg" className="" alt="whatsapp"/>
+                                <img src="/images/wechat.svg" className="" alt="whatsapp"/>
                         </div>
                         <div className="contact-info">
                             <p className="info-p">+6046575030</p>
                         </div>
                     </div>
                 </a>
-                <a href="https://www.linkedin.com/in/amirmiri/">
+                <a href="https://www.linkedin.com/in/amirmiri/" target="_blank" rel="noreferrer">
                     <div className="rounded-box">
                         <div className="icon-img">
-                                <img src="/images/Group 10.svg" className="" alt="linkedin"/>
+                                <img src="/images/linkedin.svg" className="" alt="linkedin"/>
                         </div>
                         <div className="contact-info">
                             <p className="info-p">LinkedIn/Amir Miri</p>
                         </div>
                     </div>
                 </a>
-                <a href="https://www.youtube.com/channel/UC1dh0t77e2yHQdvM-naOs0w">
+                <a href="https://www.youtube.com/channel/UC1dh0t77e2yHQdvM-naOs0w" target="_blank" rel="noreferrer">
                     <div className="rounded-box">
                         <div className="icon-img">
-                                <img src="/images/Group 11.svg" className="" alt="youtube"/>
+                                <img src="/images/youtube.svg" className="" alt="youtube"/>
                         </div>
                         <div className="contact-info">
                             <p className="info-p">YouTube/Amir Miri</p>
                         </div>
                     </div>
                 </a>
-                <a href="https://www.google.com/maps/place/1200+W+73rd+Ave+%23100,+Vancouver,+BC+V6P+6G5,+Canada/@49.2040785,-123.1332686,17z/data=!3m1!4b1!4m5!3m4!1s0x548674e9462aae17:0x7c51aa4d938e201b!8m2!3d49.2040785!4d-123.1332686">
+                <a href="https://www.google.com/maps/place/1200+W+73rd+Ave+%23100,+Vancouver,+BC+V6P+6G5,+Canada/@49.2040785,-123.1332686,17z/data=!3m1!4b1!4m5!3m4!1s0x548674e9462aae17:0x7c51aa4d938e201b!8m2!3d49.2040785!4d-123.1332686" target="_blank" rel="noreferrer">
                     <div className="rounded-box">
                         <div className="icon-img">
-                                <img src="/images/Group 12.svg" className="" alt="address"/>
+                                <img src="/images/location.svg" className="" alt="address"/>
                         </div>
                         <div className="contact-info">
-                            <p className="info-p">#100-1200 West 73rd Avenue, Vancouver,  BC V6P 6G5</p>
+                            <p className="info-p special-text">#100-1200 West 73rd Avenue, Vancouver,  BC V6P 6G5</p>
                         </div>
                     </div>
                 </a>
-                <div className="button-row">
+                <div className="button-row sticky-btn">
                 <button className="contact-btn">Save Contact</button> 
                 </div>               
             </div>
